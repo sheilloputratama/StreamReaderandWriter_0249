7d4271832758a3d7e002fbb53b1acc951ecd135d
sheilloputratama <sheilloggendut@gmail.com>
2025-06-09 18:41:09 +0700

    dan tampilan disini

--- ioFile.cpp
+++ ioFile.cpp
@@ -42,4 +42,8 @@ int main()
     {
         // melakukan perulangan setiap baris
         while (getline(infile, baris))
+        {
+            // dan tampilan disini
+            cout << baris << '\n';
+        }
         
